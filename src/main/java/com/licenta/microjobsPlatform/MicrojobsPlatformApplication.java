ab494eaed7fa1b3c76e9125b7a002befcb7e3292
Matthew Raul <raulmihmatei1@gmail.com>
2026-03-21 01:46:48 +0200

    initial backend

---
+++ src/main/java/com/licenta/microjobsPlatform/MicrojobsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.licenta.microjobsPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrojobsPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrojobsPlatformApplication.class, args);
+	}
+
+}
